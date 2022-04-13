68ab60cb19c885f8c91495c2cb1c1223da910cf6
Joe <joedinsley31@gmail.com>
2022-04-13 15:25:05 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="flexycont">
+
+
+    <table class="scoreboardtable">
+        <tr>
+            <td class="tableHeader">Sales</td>
+            <td class="tableHeader">Direct Debits</td>
+            <td class="tableHeader">Cash</td>
+            <td class="tableHeader">Upfront Payments</td>
+            <td class="tableHeader">New Sales</td>
+            <td class="tableHeader">Total</td>
+        </tr>
+        <tr>
+            <td class="SalesPerson">Dan</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td class="SalesPerson">Dory</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td class="SalesPerson">Chelsea</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td class="SalesPerson">Robert</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td class="SalesPerson">Leona</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+
+    </div>
+</body>
+
+</html>
